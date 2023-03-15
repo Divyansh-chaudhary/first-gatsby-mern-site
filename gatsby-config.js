4ed3f5cc7e25a2d9922d7cffdceecb19d810a274
DivyanshChaudhary17 <divyansh.chaudhary@oodles.io>
2023-03-15 17:33:58 +0530

    try antd css

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,5 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-antd',
-      options: {
-        style: true
-      }
-    }
-  ]
+  plugins: ['gatsby-plugin-less']
 };
